a91c03875abe514198edf338c6856e1adbdf956e
Stiven106 <brayan106-@hotmail.com>
2021-04-01 10:41:23 -0500

    Create TeoriaConceptosJSPs.java

---
+++ src/JSPsConceptos/TeoriaConceptosJSPs.java
@@ -0,0 +1,48 @@
+package JSPsConceptos;
+
+public class TeoriaConceptosJSPs {
+    /*
+                    CONCEPTOS BASICOS DE JAVA SERVER PAGES
+
+        Son componentes del lado del servidor web, estos componentes estan especializados en manejar codigo HTML y a su vez inscrustar codigo Java
+        por medio de etiquetas, conocidas como tags. Los JSPs son utilizados como componentes de presentacion, es decir, nos permite mostrar la
+        informacion procesada como pueden ser los Servlets.
+
+                    BENEFICIOS DE LOS JSPs
+
+        *Enfocarse en escribir codigo HTML, haciendo mas facil el mantenimiento de la capa de presentacion.
+
+        *Se pueden utilizar herramientas de diseño para crear visualmente las paginas HTML e incrustar las etiquetas dinamicas de los JSP's.
+
+        *Separa el codigo de presentacion del codigo de Java.
+
+        *Cada miembro del equipo de desarrolo se puede enfocar en distintas tareas, al separar las responsabilidades.
+
+                    FUNCIONES DE UN JSP
+
+        Tiene codigo HTML
+
+        Se utiliza como un componente de presentacion.
+
+        Var a permitir mostrar la informacion que reciban del Servlet, y a su vez enviar informacion al Servlet.
+
+                CICLO DE VIDA DE UN JSP
+
+        1. Se traduce el JSP a un Servlet >>> Se compila y se carga la clase del Servlet
+
+        2.Se crea un subproceso del Servlet
+
+        3. Se ejecuta el metodo init()
+
+        4. Se ejecuta el metodo service()
+
+        5. Se ejecuta el metodo destroy()
+
+     */
+
+
+
+
+
+
+}
